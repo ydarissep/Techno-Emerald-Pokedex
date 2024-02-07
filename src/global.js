0ff57c7a18853cc7b4d146c1f987346376a04a76
ydarissep <60859033+ydarissep@users.noreply.github.com>
2024-02-07 11:59:42 +0100

    Add files via upload

--- src/global.js
+++ src/global.js
@@ -1,5 +1,5 @@
 window.repo = "MythicalBlueFlare/techno-emerald/main"
-window.checkUpdate = "9 TE"
+window.checkUpdate = "10 TE"
 
 
 fetch('https://raw.githubusercontent.com/ydarissep/dex-core/main/index.html').then(async response => {
